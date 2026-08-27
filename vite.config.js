fc957d3730764e8612e1d759e92497e3a801c570
ShreyaBhat <shreyab232@gmail.com>
2026-08-27 17:08:45 +0530

    Configure Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import path from "path";
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
     react(),
+    netlify(),
     tailwindcss(),
   ],
   resolve: {
